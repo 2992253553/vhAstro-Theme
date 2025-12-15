3d88ae2d835b85f45e7f8326445661587d171592
Privat5418 <150410216+2992253553@users.noreply.github.com>
2025-12-15 15:41:47 +0800

    Update config.ts

--- src/config.ts
+++ src/config.ts
@@ -1,12 +1,18 @@
 export default {
   // 网站标题
-  Title: '韩小韩博客',
+  Title: 'Privat5418博客',
   // 网站地址
   Site: 'https://www.vvhan.com',
   // 网站副标题
   Subtitle: '不曾与你分享的时间,我在进步.',
   // 网站描述
-  Description: '韩小韩博客 专注于前开发与相关技术的实战分享，涵盖Vue框架、Node.js、Serverless等，并涉及Node、Python、Linux、Docker等领域。同时，博客也分享作者的生活、音乐和旅行的热爱。',
+  Description: 'Privat5418博客 本人学识渊博，经验丰富，代码风骚，效率恐怖。C/C＋＋，java，php无不精通，熟练掌握各种框架。
+会DDOS一秒钟死一个站，会注入，会上传，会Xss，会破解，会嗅探，会开发，会业务，会运维，会渗透。互联网内，我无处不在。
+全国漏洞认识深刻，熟练掌握各种操作系统内核。认真学习过《黑客攻防技术宝典》，《逆向工程》，《游戏waigua攻防艺术》等专业书籍。深山苦练十余载，一天只睡2小时。
+听指挥，执行快，脾气好，不喷人，操作虎，意识强，渗透快，shell多，能过狗，能过盾。
+千里之外取服务器首级，瞬息之间爆管理员狗头，压安全狗如压草芥。电话通知出Bug后，秒登VPN，千里之外定位问题，瞬息之间修复上线。
+只有你想不到的，没有我做不到的。
+以上都是吹的，我只会划水摸鱼，我是个废物。',
   // 网站作者
   Author: '.𝙃𝙖𝙣',
   // 作者头像
@@ -23,7 +29,7 @@ export default {
     "I am making progress in the time I haven't shared with you.",
   ],
   // 网站创建时间
-  CreateTime: '2021-09-01',
+  CreateTime: '2025-12-15',
   // 顶部 Banner 配置
   HomeBanner: {
     enable: true,
